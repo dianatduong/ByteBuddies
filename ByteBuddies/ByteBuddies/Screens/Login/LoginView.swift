fa8acc7006f25fb3db0b0e230f8bbb1fcad728a4
Diana Duong <dduong013@gmail.com>
2024-01-11 19:49:28 -0600

    add opacity

--- ByteBuddies/ByteBuddies/Screens/Login/LoginView.swift
+++ ByteBuddies/ByteBuddies/Screens/Login/LoginView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct LoginView: View {
     
-    @State private var userNameLogin = ""
+    @State private var emailLogin = ""
     @State private var passwordLogin = ""
     
     @State private var nameSignup = ""
@@ -18,71 +18,84 @@ struct LoginView: View {
     @State private var passwordSignup = ""
     
     var body: some View {
-        
-        NavigationView {
-            VStack(alignment: .leading) {
-                Text("Welcome to Byte Buddies!")
-                    .font(.system(size: 40))
-                    .fontWeight(.bold)
-                    .padding(EdgeInsets(top: 70, leading: 30, bottom: 30, trailing: 20))
+        ZStack {
+            
+            LinearGradient(colors: [Color.cyan, Color.paleYellow], startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
 
-                Form {
-                    Section(header: Text("Login")) {
-                        TextField("Username", text: $userNameLogin)
+                Text("[Code] Haven")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+                
+
+                VStack(spacing: 15){
+                    Section(header: Text("Login       |       Create an Account")) {
+                        TextField("Email", text: $emailLogin)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(8)
+                        
                         TextField("Password", text: $passwordLogin)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(8)
+                        
                         Button(action: {
                             print("Login Button Tapped")
                         }) {
                             Text("Login")
+                                .font(.subheadline)
                                 .foregroundColor(.white)
+                                .fontWeight(.bold)
                                 .padding()
                                 .frame(maxWidth: .infinity)
                                 .background(Color.cyan)
                                 .cornerRadius(8)
                         }
                         
+                        Text("or")
+                        
                         Button(action: {
                             print("Continue with LinkedIn")
                         }) {
                             Text("Continue with LinkedIn")
+                                .font(.subheadline)
                                 .foregroundColor(.white)
+                                .fontWeight(.bold)
                                 .padding()
                                 .frame(maxWidth: .infinity)
                                 .background(Color.cyan)
                                 .cornerRadius(8)
                         }
-                    }
-              
-
-                Section(header: Text("Create an Account")) {
-                    TextField("Name", text: $nameSignup)
-                    TextField("Username", text: $userNameSignup)
-                    TextField("Password", text: $passwordSignup)
-                    Button(action: {
-                        print("Sign Up")
-                    }) {
-                        Text("Sign Up")
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .background(Color.cyan)
-                            .cornerRadius(8)
-                        }
-                    }
+                    } //end Section
                     
-                  
-                } // end Form
-              
-            } // end VStack
-            .background(LinearGradient(colors: [Color.cyan, Color.paleYellow], startPoint: .leading, endPoint: .trailing))
-            .navigationBarTitle("", displayMode: .inline)
-        } // end NavigationView
-
+                    Section {
+                        Text("Forgot your password?")
+                            .font(.subheadline)
+                            .foregroundColor(.cyan)
+                            .fontWeight(.bold)
+                    }
+                } //end vstack
+                .frame(width: 320, height: 360)
+                .padding(20)
+                .background(Color.white.opacity(0.4))
+                .cornerRadius(10)
+                .padding(.top, 15)
+            } // end vstack
+        }
     }
 }
-
+                
+     
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
     }
 }
+
+
+//insets, geometry reader - dynamic heights, uiscreens, SegmentedController
