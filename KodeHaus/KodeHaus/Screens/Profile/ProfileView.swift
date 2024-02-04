68b794cfbc1c00e68e5412259a118a4fc4087dcb
Diana Duong <dduong013@gmail.com>
2024-02-04 14:19:50 -0600

    add profile image + border

--- KodeHaus/KodeHaus/Screens/Profile/ProfileView.swift
+++ KodeHaus/KodeHaus/Screens/Profile/ProfileView.swift
@@ -22,7 +22,21 @@ struct ProfileView: View {
                 Spacer()
             }
                   
-           
+            VStack {
+                Image("dog-profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 160, height: 160)
+                    .clipShape(Circle())
+                    .overlay( //border around image
+                        Circle()
+                            .stroke(Color.magenta, lineWidth: 5)
+                    )
+                .offset(y: 55)
+                Spacer()
+            }
+            
+          
            
                  
         } // end ZStack
