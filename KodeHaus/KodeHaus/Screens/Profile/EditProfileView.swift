0f25ac7ecddf5ac8b7ee4e4be3565d52f6dc1374
Diana Duong <dduong013@gmail.com>
2024-03-06 13:15:59 -0600

    add color picker

--- KodeHaus/KodeHaus/Screens/Profile/EditProfileView.swift
+++ KodeHaus/KodeHaus/Screens/Profile/EditProfileView.swift
@@ -7,68 +7,85 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct EditProfileView: View {
-    @State private var name = ""
+    @State private var profileImage: Image? = Image("bulldog-glasses")
+    @State private var fullName = ""
     @State private var email = ""
     @State private var location = ""
     
+    @State private var primaryColor = Color.magenta1
+    @State private var secondaryColor = Color.pink
+    
     var body: some View {
-        
-
-        
-        VStack {
-            
-            Section {
-                Image("bulldog-glasses")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 160, height: 160)
-                    .clipShape(Circle())
-                
+        NavigationView {
+            VStack {
+                // Profile Picture
+                Button(action: {
+                    // Add action to change profile picture
+                }) {
+                    profileImage?
+                        .resizable()
+                       .aspectRatio(contentMode: .fill)
+                       .frame(width: 160, height: 160)
+                       .clipShape(Circle())
+                }
                 Text("Edit picture")
                     .padding(.top, 10)
-                    .font(Font.system(size: 18, weight: .bold))
-                    .foregroundColor(.magenta1)
-            }
-           
-            
-            VStack(spacing: 20) {
-                Section {
-                    
-                    CustomTextField(bindValue: $name, fieldName: "Name" , color: Color.btnGray1, frameHeight: 2, type: .text)
-                    
-                    CustomTextField(bindValue: $email, fieldName: "Email" , color: Color.btnGray1, frameHeight: 2, type: .text)
-                    
-                    CustomTextField(bindValue: $location, fieldName: "City/State" , color: Color.btnGray1, frameHeight: 2, type: .text)
+                    .font(Font.system(size: 20, weight: .bold))
+                    .foregroundColor(.blue)
+                
+                
+                //Personal Info
+                VStack(spacing: 20) {
+                    Section {
+                        CustomTextField(bindValue: $fullName, fieldName: "Name" , color: Color.btnGray1, frameHeight: 2, type: .text)
+                        
+                        CustomTextField(bindValue: $email, fieldName: "Email" , color: Color.btnGray1, frameHeight: 2, type: .text)
+                        
+                        CustomTextField(bindValue: $location, fieldName: "City/State" , color: Color.btnGray1, frameHeight: 2, type: .text)
+                    }
                 }
+               // .padding()
+                
                
                 
                 
-                Section {
-                    Text("Profile Design")
-                        .foregroundColor(Color.magenta1)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .font(Font.system(size: 18, weight: .bold))
-                       
+                
+                VStack {
+                    
+                    Text("Select your color profile:")
+                        .foregroundColor(Color.gray)
+                        .font(Font.system(size: 20, weight: .semibold))
 
+
+                    
+                    VStack(spacing:10) {
+                        ColorPicker("Primary Color", selection: $primaryColor)
+                            .foregroundColor(Color.gray)
+
+        
+                        ColorPicker("Secondary Color", selection: $secondaryColor)
+                            .foregroundColor(Color.gray)
+                           
+                    }
+                    .font(Font.system(size: 18))
                 }
-                .padding(.top, 20)
+                .padding(.top, 30)
                 
                 Spacer()
-
-                // StatePicker()
-            } //end vstack
-            .padding(.top, 20)
+                
+            }
+            .frame(width: 350)
+           // .navigationBarTitleDisplayMode("Edit Profile")
         }
-        .frame(width: 350)
-        
-
     }
 }
 
-
-struct UpdateProfileView_Previews: PreviewProvider {
+struct EditProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        UpdateProfileView()
+        EditProfileView()
     }
 }
+
