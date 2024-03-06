c0d8f1a7a09ddc1cf4550d30df0672c6c0dda243
Diana Duong <dduong013@gmail.com>
2024-03-06 13:33:53 -0600

    edit ui for EditProfileView

--- KodeHaus/KodeHaus/Screens/Profile/EditProfileView.swift
+++ KodeHaus/KodeHaus/Screens/Profile/EditProfileView.swift
@@ -21,65 +21,66 @@ struct EditProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
+                
                 // Profile Picture
-                Button(action: {
-                    // Add action to change profile picture
-                }) {
-                    profileImage?
-                        .resizable()
-                       .aspectRatio(contentMode: .fill)
-                       .frame(width: 160, height: 160)
-                       .clipShape(Circle())
+                Section {
+                    Button(action: {
+                        // Add action to change profile picture
+                    }) {
+                        profileImage?
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 160, height: 160)
+                            .clipShape(Circle())
+                    }
+                    Text("Edit picture")
+                        .padding(.top, 10)
+                        .font(Font.system(size: 18, weight: .bold))
+                        .foregroundColor(.blue)
                 }
-                Text("Edit picture")
-                    .padding(.top, 10)
-                    .font(Font.system(size: 20, weight: .bold))
-                    .foregroundColor(.blue)
-                
-                
-                //Personal Info
+
                 VStack(spacing: 20) {
-                    Section {
-                        CustomTextField(bindValue: $fullName, fieldName: "Name" , color: Color.btnGray1, frameHeight: 2, type: .text)
-                        
-                        CustomTextField(bindValue: $email, fieldName: "Email" , color: Color.btnGray1, frameHeight: 2, type: .text)
-                        
-                        CustomTextField(bindValue: $location, fieldName: "City/State" , color: Color.btnGray1, frameHeight: 2, type: .text)
-                    }
-                }
-               // .padding()
-                
-               
-                
-                
-                
-                VStack {
-                    
-                    Text("Select your color profile:")
-                        .foregroundColor(Color.gray)
-                        .font(Font.system(size: 20, weight: .semibold))
-
-
-                    
                     VStack(spacing:10) {
-                        ColorPicker("Primary Color", selection: $primaryColor)
-                            .foregroundColor(Color.gray)
-
-        
-                        ColorPicker("Secondary Color", selection: $secondaryColor)
-                            .foregroundColor(Color.gray)
-                           
+                        
+                        //Personal Info
+                        Section(header:
+                                    Text("Personal Info")
+                            .textCase(.uppercase)
+                        ) {
+                            
+                            CustomTextField(bindValue: $fullName, fieldName: "Name" , color: Color.btnGray1, frameHeight: 2, type: .text)
+                            
+                            CustomTextField(bindValue: $email, fieldName: "Email" , color: Color.btnGray1, frameHeight: 2, type: .text)
+                            
+                            CustomTextField(bindValue: $location, fieldName: "City/State" , color: Color.btnGray1, frameHeight: 2, type: .text)
+                        }
                     }
-                    .font(Font.system(size: 18))
+                    .padding(.top, 40)
+                    
+ 
+                    VStack(spacing: 10) {
+                        
+                        //Color Profile
+                        Section(header:
+                                    Text("Select color profile:")
+                                        .textCase(.uppercase)
+                        ) {
+                            
+                            ColorPicker("Primary Color", selection: $primaryColor)
+                                .foregroundColor(Color.gray)
+                            
+                            ColorPicker("Secondary Color", selection: $secondaryColor)
+                                .foregroundColor(Color.gray)
+                            
+                        }
+                        .font(Font.system(size: 18))
+                    }
+                    .padding(.top, 40)
                 }
-                .padding(.top, 30)
-                
-                Spacer()
-                
+             Spacer()
             }
             .frame(width: 350)
-           // .navigationBarTitleDisplayMode("Edit Profile")
-        }
+g        }
     }
 }
 
