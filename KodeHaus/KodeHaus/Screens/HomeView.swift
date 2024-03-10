ba08906877997eb87d40abfc4d8220c91f8e1dac
Diana Duong <dduong013@gmail.com>
2024-03-10 14:57:51 -0500

    tab views

---
+++ KodeHaus/KodeHaus/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  KodeHaus
+//
+//  Created by Diana Duong on 3/10/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        Text("Home")
+    }
+}
+
+
+#Preview {
+    HomeView()
+}
--- KodeHaus/KodeHaus/Screens/TabBar.swift
+++ KodeHaus/KodeHaus/Screens/TabBar.swift
@@ -7,23 +7,56 @@
 
 import SwiftUI
 
+enum Tab: String, CaseIterable {
+    case house
+    case message
+    case person
+    case leaf
+    case gearshape
+}
+
+
+
 struct TabBar: View {
+    @Binding var selectedTab: Tab
+    private var fillImage: String {
+        selectedTab.rawValue + ".fill"
+    }
+    
     var body: some View {
         
-        TabView {
-            
-            ProfileViewPink()
-                .tabItem {
-                    Image(systemName: "person")
-                    Text("Profile")
+        VStack {
+            HStack {
+                ForEach(Tab.allCases, id: \.rawValue) { tab in
+                    
+                    Spacer()
+                    
+                    //use fill image when selected
+                    Image(systemName: selectedTab  == tab ? fillImage : tab.rawValue)
+                    
+                        //enlarge icon when selected
+                        .scaleEffect(selectedTab == tab ? 1.25 : 1.0)
+                    
+                        .foregroundColor(selectedTab == tab ? .red : .gray)
+                    
+                        .font(.system(size: 22))
+                        .onTapGesture {
+                            withAnimation(.easeIn(duration: 0.1)) {
+                                selectedTab = tab
+                            }
+                        }
+                    Spacer()
                 }
-         
+                
+            } // end Hstack
+            .frame(width: nil, height: 60)
+            .background(.thinMaterial)
+            .cornerRadius(10)
+            .padding()
         }
-      
-
     }
 }
 
 #Preview {
-    TabBar()
+    TabBar(selectedTab: .constant(.message))
 }
